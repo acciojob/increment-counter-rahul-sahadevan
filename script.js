6fc199ea1d67f7a0b6c3b794ff2e20c84c1aa16f
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-24 14:41:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const inc = document.getElementById("incrementBtn");
+let incre = 1;
+inc.addEventListener("click",() =>{
+	let val = document.getElementById("counter");
+	alert(val.innerText);
+    val.innerText = incre;
+	let prev = val.innerText;
+	incre++;
+	
+})
